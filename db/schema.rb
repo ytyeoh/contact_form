bfcb38d6a8f409cfdd5ca1c1d331d7e53cb01104
ytyeoh <ytyeoh@me.com>
2019-10-21 09:48:05 +0800

    change datatbase

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_10_19_002005) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lists", force: :cascade do |t|
     t.string "name"
     t.string "email"
