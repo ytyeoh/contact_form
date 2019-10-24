57bffb282ebe199b88ef53e6b1c4fe5fb3c4b50d
ytyeoh <ytyeoh@me.com>
2019-10-24 17:06:42 +0800

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :lists
   devise_for :users
   root 'lists#index'
+  mount_griddler
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
